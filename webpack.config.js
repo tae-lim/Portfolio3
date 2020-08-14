2f51d1911551f82ff7ccea5941acbe26044251b7
tae-lim <taelim92@gmail.com>
2020-08-14 16:40:40 -0700

    Add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,24 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
       }
     ]
   },
